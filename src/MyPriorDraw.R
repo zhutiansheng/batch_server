b953379b5b5128c632a93b2bf8839271409b7f02
zhutiansheng <44465877+zhutiansheng@users.noreply.github.com>
2019-08-16 07:10:01 +0800

    Update MyPriorDraw.R

--- src/MyPriorDraw.R
+++ src/MyPriorDraw.R
@@ -1,3 +1,5 @@
+priorData<-result$additiondata
+batchName="2"
 drawPrior<-function(priorData,batchName="2") {
   gamma.hat<-priorData$gamma.hat
   delta.hat<-priorData$delta.hat
@@ -11,24 +13,23 @@ drawPrior<-function(priorData,batchName="2") {
   lwd=2
   myColor = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")
   tmp <- density(gamma.hat[batchNum, ])
-  plot(tmp, type = "l",bty="l",las=1,lwd=lwd, col=myColor[5], main = expression(paste("Density Plot of Batch for ",
-                                                hat(gamma))))
+  plot(tmp, type = "l",bty="l",las=1,lwd=lwd, col=myColor[5], main = substitute(paste("Density Plot of Batch '",batchName,"' for ",
+                                                                                      hat(gamma)),list(batchName=batchName)))
   xx <- seq(min(tmp$x), max(tmp$x), length = 100)
   lines(xx, dnorm(xx, gamma.bar[batchNum], sqrt(t2[batchNum])),lwd=lwd,  col = myColor[6])
-  qqnorm(gamma.hat[batchNum, ],bty="l",las=1, lwd=lwd, col=myColor[2],main = expression(paste("Normal Q-Q Plot of Batch for ",
-                                                 hat(gamma))))
+  qqnorm(gamma.hat[batchNum, ],bty="l",las=1, lwd=lwd, col=myColor[2],main = substitute(paste("Normal Q-Q Plot of Batch '",batchName,"' for ",
+                                                                                              hat(gamma)),list(batchName=batchName)))
   qqline(gamma.hat[batchNum, ], lwd=lwd, col = myColor[6])
   tmp <- density(delta.hat[batchNum, ])
   xx <- seq(min(tmp$x), max(tmp$x), length = 100)
   tmp1 <- list(x = xx, y = dinvgamma(xx, a.prior[batchNum], b.prior[batchNum]))
   plot(tmp, type = "l",bty="l",las=1, ylim = c(0, max(tmp$y, tmp1$y)),lwd=lwd, col=myColor[5],
-       main = expression(paste("Density Plot of Batch for ",
-                               hat(delta))))
+       main = substitute(paste("Density Plot of Batch '",batchName,"' for ",
+                               hat(delta)),list(batchName=batchName)))
   lines(tmp1,lwd=lwd,  col = myColor[6])
   invgam <- 1/qgamma(1 - ppoints(ncol(delta.hat)), a.prior[batchNum],
                      b.prior[batchNum])
-  qqplot(invgam, delta.hat[batchNum, ],bty="l",las=1, lwd=lwd, col = myColor[2], main = expression(paste("Inverse Gamma Q-Q Plot of Batch for ",
-                                                         hat(delta))), ylab = "Sample Quantiles", xlab = "Theoretical Quantiles")
+  qqplot(invgam, delta.hat[batchNum, ],bty="l",las=1, lwd=lwd, col = myColor[2], main = substitute(paste("Inverse Gamma Q-Q Plot of Batch '",batchName,"' for ",
+                                                                                                         hat(delta)),list(batchName=batchName)), ylab = "Sample Quantiles", xlab = "Theoretical Quantiles")
   lines(c(0, max(invgam)), c(0, max(invgam)), lwd=lwd, col = myColor[6])
 }
-
