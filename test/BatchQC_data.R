7e1496ea295306b5d5f2d7270f38d5b5eea63853
zhutiansheng <tszhu@fudan.edu.cn>
2019-09-10 23:14:11 +0800

    modify BatchQC_data

--- test/BatchQC_data.R
+++ test/BatchQC_data.R
@@ -1,15 +1,32 @@
-library(GEOquery)
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("GEOquery")
-BiocManager::install("proBatch")
-BiocManager::install("GEOquery")
+source("global.R")
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install("BatchQC")
+
 
 library(BatchQC)
 #dataset 1
 data(example_batchqc_data)
 batch <- batch_indicator$V1
 condition <- batch_indicator$V2
+
+system.time(
+  d1.combat<-ComBat(as.matrix(signature_data),batch,mod = NULL)
+)
+drawUMAP<-function(myd,label){
+  myd[is.na(myd)]<-0
+  myumap<-umap(myd,n_neighbors=10)
+  mydf<-data.frame(myumap$layout)
+  mydf$label<-label
+  p<-ggplot(mydf,aes(x=X1, y=X2, colour=label)) + geom_point(size=3)+
+    theme(  #panel.grid.major = element_blank(),
+      #panel.grid.minor = element_blank(),
+      #axis.text.x = element_text(vjust = 1,angle = 45),
+      axis.line.x = element_line(color="black", size = 0.5),
+      axis.line.y = element_line(color="black", size = 0.5),
+      panel.background = element_blank())
+  print(p)
+}
 
 #dataset 2
 data(bladderdata)
