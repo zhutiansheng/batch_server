64b35ee35291bcb09e8931ce9a1f126668faa296
zhutiansheng <tszhu@fudan.edu.cn>
2019-09-11 22:33:46 +0800

    add fun

--- test/BatchQC_data.R
+++ test/BatchQC_data.R
@@ -2,17 +2,6 @@ source("global.R")
 # if (!requireNamespace("BiocManager", quietly = TRUE))
 #   install.packages("BiocManager")
 # BiocManager::install("BatchQC")
-
-
-library(BatchQC)
-#dataset 1
-data(example_batchqc_data)
-batch <- batch_indicator$V1
-condition <- batch_indicator$V2
-
-system.time(
-  d1.combat<-ComBat(as.matrix(signature_data),batch,mod = NULL)
-)
 drawUMAP<-function(myd,label){
   myd[is.na(myd)]<-0
   myumap<-umap(myd,n_neighbors=10)
@@ -28,7 +17,37 @@ drawUMAP<-function(myd,label){
   print(p)
 }
 
+library(BatchQC)
+
+analysisBatch<-function(dat,batch,type,sample_info,out_pdf){
+  pdf(out_pdf)
+  pvcaobj<-pvcaBF(dat,sample_info,c("batch","type"),0.1)
+  pieDraw(pvcaobj)
+  drawUMAP(t(dat),type)
+  drawUMAP(t(dat),as.factor(batch))
+  
+  modcombat = model.matrix(~as.factor(type), data=sample_info)
+  
+  for (param in c("auto","parameter","noparameter")) {
+    t<-system.time(dat.combat<-combat(as.matrix(dat),batch,mod = modcombat,par.prior=param))
+    message(param)
+    message(t)
+  }
+  dev.off()
+}
+#dataset 1
+data(example_batchqc_data)
+batch <- batch_indicator$V1
+condition <- batch_indicator$V2
+
+system.time(
+  d1.combat<-ComBat(as.matrix(signature_data),batch,mod = NULL)
+)
+
+#####
 #dataset 2
+#####
+
 data(bladderdata)
 pheno <- pData(bladderEset)
 edata <- exprs(bladderEset)
