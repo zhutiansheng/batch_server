cb7066dd67a9f43211cf31ea681fd949be1a6cba
Maciej Motyka <maciej.motyka@ardigen.com>
2020-10-25 21:24:23 +0100

    Don't specify port for the shiny app. See https://github.com/zhutiansheng/batch_server/issues/1\#issuecomment-662416186

--- app.R
+++ app.R
@@ -3,7 +3,6 @@ options(shiny.trace = TRUE)
 options(shiny.fullstacktrace = TRUE)
 options(shiny.error = browser)
 shiny::runApp(
-  port = 80,
   display.mode = "auto",
   host = getOption("shiny.host", "0.0.0.0"),
   quiet = F,
